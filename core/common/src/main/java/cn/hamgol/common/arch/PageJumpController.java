f730b1ec8fcb9cad221c28cdae13e4b65c32202c
hamgol <hamgol@163.com>
2018-11-05 15:19:14 +0800

    add java core
    add java core

---
+++ core/common/src/main/java/cn/hamgol/common/arch/PageJumpController.java
@@ -0,0 +1,34 @@
+package cn.hamgol.common.arch;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 公共URL跳转
+ * @author hamgol
+ */
+@Controller
+@RequestMapping(value = "/page", method = {RequestMethod.GET, RequestMethod.POST})
+public class PageJumpController {
+
+	private static final Logger log = LoggerFactory.getLogger(PageJumpController.class);
+	
+	@RequestMapping("/{path}/{page}")
+	public String urlJump(@PathVariable(value="path") String path, @PathVariable(value="page") String page, HttpServletRequest req) {
+		
+		String url = req.getServletPath();
+		
+		log.info("url jump");
+		log.info("url  : " + url);
+		log.info("path : " + path);
+		log.info("page : " + page);
+		
+		return path + "/" + page;
+	}
+}
